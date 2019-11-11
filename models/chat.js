fa0d56ee485f39fdc06ebe55f69a17df66923f48
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-11-11 14:06:53 -0500

    updated folders in chatroom, updated server.js

--- models/chat.js
+++ models/chat.js
@@ -2,22 +2,22 @@
 var orm = require("../config/orm.js");
 var cat = {
   all: function(cb) {
-    orm.all("cats", function(res) {
+    orm.all("chat", function(res) {
       cb(res);
     });
   },
   // cols and vals are arrays
   create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+    orm.create("chat", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+    orm.update("chat", objColVals, condition, function(res) {
       cb(res);
     });
   }
 };
 
-/* EXPORT ORM FUNCTIONS FOR CATSCONTROLLER */
-module.exports = cat;
+// Export the database functions for the controller (chatController.js).
+module.exports = chat;
